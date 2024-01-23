24881125a31e4cfe80c28773032884237d07fafa
gaintopskills <raynafit@yahoo.com>
2024-01-23 08:38:10 -0800

    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://samedayappliance.repair",
   integrations: [tailwind(), mdx(), sitemap()],
 });
+
+
