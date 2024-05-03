6ba13b8a93aa1c9dd85e752d84686c622e0e64b5
Alex-root-ru <al_ratavnin@mail.ru>
2024-05-03 23:17:01 +0400

    y

--- src/js/price/price.js
+++ src/js/price/price.js
@@ -1,15 +1,17 @@
 // Часто задаваемые вопросы
-document.addEventListener('DOMContentLoaded', function () {
-    let coll = document.getElementsByClassName('FAQ');
-    for (let i = 0; i < coll.length; i++) {
-      coll[i].addEventListener('click', function () {
-        this.classList.toggle('active');
-        let content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-          content.style.maxHeight = null;
-        } else {
-          content.style.maxHeight = content.scrollHeight + 'px';
-        }
-      });
-    }
-  });
+// document.addEventListener('DOMContentLoaded', function () {
+//     let coll = document.getElementsByClassName('FAQ');
+//     for (let i = 0; i < coll.length; i++) {
+//       coll[i].addEventListener('click', function () {
+//         this.classList.toggle('active');
+//         let content = this.nextElementSibling;
+//         if (content.style.maxHeight) {
+//           content.style.maxHeight = null;
+//         } else {
+//           content.style.maxHeight = content.scrollHeight + 'px';
+//         }
+//       });
+//     }
+//   });
+
+alert("triymf");
