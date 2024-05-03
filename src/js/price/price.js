13496fac49c57775d1e20b6e9ef48ed37371045b
Alex-root-ru <al_ratavnin@mail.ru>
2024-05-03 23:21:47 +0400

    y

--- src/js/price/price.js
+++ src/js/price/price.js
@@ -1,17 +1,15 @@
 // Часто задаваемые вопросы
-// document.addEventListener('DOMContentLoaded', function () {
-//     let coll = document.getElementsByClassName('FAQ');
-//     for (let i = 0; i < coll.length; i++) {
-//       coll[i].addEventListener('click', function () {
-//         this.classList.toggle('active');
-//         let content = this.nextElementSibling;
-//         if (content.style.maxHeight) {
-//           content.style.maxHeight = null;
-//         } else {
-//           content.style.maxHeight = content.scrollHeight + 'px';
-//         }
-//       });
-//     }
-//   });
-
-alert("triymf");
+document.addEventListener('DOMContentLoaded', function () {
+    let coll = document.getElementsByClassName('FAQ');
+    for (let i = 0; i < coll.length; i++) {
+      coll[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + 'px';
+        }
+      });
+    }
+  });
