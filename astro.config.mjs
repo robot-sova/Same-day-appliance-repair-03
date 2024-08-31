44cc4dd7b32613a0032e4915ff13a8e731410111
Alex-root-ru <al_ratavnin@mail.ru>
2024-08-31 23:16:52 +0400

    upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://samedayappliance.repair",
   integrations: [tailwind(), mdx(), sitemap()],
 
+
+  redirects: {
+    '/services/microwave-repair-los-angeles/services/induction-cooktop-repair.astro': '/services/microwave-repair-los-angeles.astro'
+  }
 
 });
 
