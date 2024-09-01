1fb03809d44a7cd1829056c10e92c1658c8797f5
Alex-root-ru <al_ratavnin@mail.ru>
2024-09-01 18:12:21 +0400

    test red

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,11 +10,6 @@ export default defineConfig({
   site: "https://samedayappliance.repair",
   integrations: [tailwind(), mdx(), sitemap()],
 
-
-  redirects: {
-    '/services/microwave-repair-los-angeles/services/induction-cooktop-repair.astro': '/services/microwave-repair-los-angeles.astro'
-  }
-
 });
 
 
